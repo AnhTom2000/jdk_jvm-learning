ad5f5ecc7663a7bf8a41683123ae6902e2221a90
AnhTom2000 <48341391+AnhTom2000@users.noreply.github.com>
2020-03-23 22:23:06 +0800

    新增jvm&操作数栈

---
+++ src/main/java/com/github/anhTom2000/StackStruTest.java
@@ -0,0 +1,17 @@
+package com.github.anhTom2000;
+
+/**
+ * Created on 19:54  23/03/2020
+ * Description:
+ *
+ * @author Weleness
+ */
+
+public class StackStruTest {
+    public static void main(String[] args) {
+        //int i = 2+3;
+        int i = 2;
+        int j = 3;
+        int k = i+j;
+    }
+}
